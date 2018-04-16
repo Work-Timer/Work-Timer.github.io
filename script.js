9e563497990a84fc5416937bd8989c1b000122d4
yschgroup <32958487+YSCHGroup@users.noreply.github.com>
2018-04-16 22:13:01 +0200

    update

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-this.onmessage = function(m) {
-  if (m.data.current !== undefined) {
-    this.postMessage({result: (+m.data.current + 1)});
-  }
+var run = false;
+var time = 0;
+function getTime() {
+	return Date().split(' ')[4].split(':')[1];
+}
+var oldDateTime = getTime();
+
+while(true) {
+	this.onmessage = function(m) {
+		if (m.data !== undefined) {
+			if (m.data == "start") {
+				run = true;
+				console.log("Started");
+			}
+			else if (m.data == "pause") {
+				run = false;
+				console.log("Paused");
+			}
+			else if (m.data == "reset") {
+				run = false;
+				time = 0;
+				console.log("Resetted");
+			}
+		}
+	}
+
+	if (run) {
+		if (oldDateTime != getTime()) {
+			oldDateTime = getTime();
+			time++;
+			this.postMessage(time);
+		}
+		console.log(time);
+	}
 }
